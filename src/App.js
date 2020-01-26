354b9f9dbcf074910663de7733282b67522b8c25
Doga Tuna <doga.tuna@gmail.com>
2020-01-26 23:38:30 +0300

    auth functionality added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import netlifyIdentity from 'netlify-identity-widget';
 
 function App() {
+  const handleLogIn = () => {
+    netlifyIdentity.open();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <h2>Welcome to React</h2>
+      </div>
+      <p className='App-intro'>
+        To get started, edit <code>src/App.js</code> and save to reload.
+      </p>
+      <div>
+        <button onClick={handleLogIn}>Log in with netlify</button>
+      </div>
     </div>
   );
 }
